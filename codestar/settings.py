91472b824d1dd38a350d8bddf1530c3020a24000
David O'Brien <ddrobrien@icloud.com>
2022-12-13 13:55:51 +0000

    simple task added to settings.py file from CI

--- codestar/settings.py
+++ codestar/settings.py
@@ -139,3 +139,7 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Account email verification (requested by Code Institute)
+
+ACCOUNT_EMAIL_VERIFICATION = 'none'
